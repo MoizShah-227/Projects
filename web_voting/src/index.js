15cccd7c66bb829c20694b21dd4ec920fb39aae3
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2025-06-23 01:35:30 +0500

    index.js is working

--- web_voting/src/index.js
+++ web_voting/src/index.js
@@ -1,17 +1,41 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { WagmiConfig, createConfig, http } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { sepolia } from 'wagmi/chains';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const chains = [sepolia];
+
+const { connectors } = getDefaultWallets({
+  appName: 'VoteChain dApp',
+  projectId: 'fe9aeaa240ab7677b3f8c101e16c1407', // from walletconnect cloud
+  chains,
+});
+
+const config = createConfig({
+  connectors,
+  publicClient: http(), // default public viem client
+  chains,
+  ssr: false,
+  autoConnect: true,
+});
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
