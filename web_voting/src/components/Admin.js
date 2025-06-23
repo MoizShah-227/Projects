9de8ba36caf94c93edf6fe4d49a0b9a2864649f4
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2025-06-23 19:37:40 +0500

    done

--- web_voting/src/components/Admin.js
+++ web_voting/src/components/Admin.js
@@ -11,8 +11,6 @@ import voteAbi from '../abi/VoteChain.json';
 import AddCandidateModal from '../components/AddCandidateModal';
 import SetVotingTimeModal from '../components/SetVotingTimeModal';
 import LandingAnimation from './LoadingAnimation';
-
-
 const contractAddress = '0xBE7DA091f727239b3edefd259195630c906c6274';
 
 const Admin = () => {
